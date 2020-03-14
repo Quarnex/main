7298d30bbe78e622a9c779dce3a4f8b46cdcc66e
Monstep <33998572+Monstep@users.noreply.github.com>
2020-03-14 12:19:46 +0500

    Вынес css в отдельные классы
    0.0.1

--- maps/maps/src/App-meny.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div id="app-meny">
-       
-    </div>
-</template>
--- maps/maps/src/components/sidepanel.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div style="width:190px;height:190px;background: aqua;">
-        <lable>Инструменты</lable>
-        <input>
-    </div>
-</template>
